0669196f21ba62563c60ac5deb5129dfc0873865
biflpansiyon-cloud <biflpansiyon@gmail.com>
2025-11-25 12:11:08 +0300

    Update menu.py

--- modules/menu.py
+++ modules/menu.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from utils import * def generate_smart_menu(month, year, pool, holidays, snacks):
-    # ... (Eski app.py'deki menü algoritması) ...
+import random
+import io
+from modules.utils import *
+
+def generate_smart_menu(month_index, year, pool, holidays, ready_snack_days):
+    # ... (V21'deki generate_smart_menu içeriği) ...
+    pass
+
+def get_full_menu_pool(client):
+    # ... (V21'deki get_full_menu_pool içeriği) ...
     pass
 
 def render_page(sel_model):

9e7a29364662c099f39ae81873c08d33cbe94e8b
biflpansiyon-cloud <biflpansiyon@gmail.com>
2025-11-25 12:01:38 +0300

    Create finans.py

---
+++ modules/finans.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from googleapiclient.discovery import build
+from utils import * def render_page(sel_model):
+    st.header("💰 Öğrenci Gelir Takip")
+    # ... (Finans UI kodları) ...

5edff55d70ef3775b95fd424fe754d1f20f060be
biflpansiyon-cloud <biflpansiyon@gmail.com>
2025-11-25 12:01:23 +0300

    Create menu.py

---
+++ modules/menu.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+from utils import * def generate_smart_menu(month, year, pool, holidays, snacks):
+    # ... (Eski app.py'deki menü algoritması) ...
+    pass
+
+def render_page(sel_model):
+    st.header("👨‍🍳 Şefin Defteri")
+    # ... (Menü UI kodları) ...
